ffb248f9804009c659c24626fa6bf2935b6ec885
hks_74123 <56163188+Gamstd74@users.noreply.github.com>
2021-04-06 22:23:22 +0530

    Delete color1.js

--- color1.js
+++
@@ -1,9 +0,0 @@
-// second part
-
-var score = sessionStorage.getItem("ss");
-
-function restart(){
-  open("https://gamstd74.github.io/My_Color_Game/","_self");
-}
-let fs=document.getElementById("yscore")
-fs.innerText=score;
